2214a846d2608d360a54f26929daae3148d05450
Anurag Kanoria <anuragkanoria15@gmail.com>
2021-07-13 15:21:22 +0530

    something

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  //const { host } = window.location;
+  //const splitHost=host.split(".");
+  
+
+  
+  res.status(200).json({"data":"Hope this data works"});
 }
